5a963f0fc62e297e912a39bd049bd8821480ff30
Milad Raeisi <miladsoft@yahoo.com>
2018-10-26 13:38:27 +0330

    upload project ...

---
+++ InstaSharpApi/Models/ErrorResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace InstaSharpApi.Models
+{
+    public class ErrorResponse : EventArgs
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ InstaSharpApi/Models/NormalResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace InstaSharpApi.Models
+{
+    public class NormalResponse : EventArgs
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
